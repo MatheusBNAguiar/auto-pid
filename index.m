c2633281a81f7928e8cf35999d42451c3d6f5c59
Matheus Barreto <matheus.bnaguiar@gmail.com>
2018-11-18 18:45:15 -0200

    Move archives

--- index.m
+++ index.m
@@ -2,3 +2,9 @@ timeSimulation = 10;
 sampleTime = 0.001;
 
 [discreteNum, discreteDen] = buildTransferFunction(sampleTime);
+
+for simulation = 1:simulationTries
+    [outputs,inputs] = runProcess(Kc,Ti,Td)
+    analyzeProcess()
+    
+end
